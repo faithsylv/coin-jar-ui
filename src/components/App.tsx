2cd1ad55144f7a62eb16a67918c60fa415426d58
Faith Sylvia <faith.sylvia@envato.com>
2022-05-24 14:32:21 +1000

    Add jars page as home page and clean up pages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,26 +5,10 @@ import Nav from "./common/Nav"
 import { Outlet } from "react-router-dom"
 
 function App() {
-  const [user, setUser] = useState({
-    name: "",
-    email: "",
-    color: ""
-  })
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetch('http://localhost:3001/users/2')
-      const json = await data.json()
-      setUser({name: json.name, email: json.email, color: json.color})
-    }
-    fetchData()
-  }, []);
-
   return (
     <div>
       <Nav />
       <Outlet />
-      <p>{user.name}</p>
     </div>
   );
 }
--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,6 +1,22 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Nav() {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    color: ""
+  })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch('http://localhost:3001/users/2')
+      const json = await data.json()
+      setUser({name: json.name, email: json.email, color: json.color})
+    }
+    fetchData()
+  }, []);
+
   return (
     <div>
       <Link to="/" style={{ textDecoration: 'none' }}>
@@ -14,6 +30,7 @@ export default function Nav() {
       >
         <Link to="/find-friends">Find Friends</Link> |{" "}
         <Link to="/create-jar">Create Jar</Link>
+        <p style={{float:'right'}}>{user.name.toUpperCase()}</p>
       </nav>
     </div>
   );
