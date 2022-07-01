16321d71d114423152bd1b9c24a396050e57b1a1
Faith Sylvia <faith.sylvia@envato.com>
2022-07-01 12:22:23 +1000

    add ability to deposit funds

--- src/components/Jar.tsx
+++ src/components/Jar.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 type JarProps = {
   jar: JarObject
 }
@@ -10,11 +12,36 @@ type JarObject = {
 }
 
 export default function Jar({ jar }: JarProps) {
+  const [depositAmount, setDepositAmount] = useState(0)
+  const [balance, setBalance] = useState(jar.balance)
+
+  console.log(typeof(jar.balance))
+
+  const handleAmountChange = (event: any) => {
+    if(parseInt(event.target.value)) {
+      setDepositAmount(parseInt(event.target.value))
+    }
+    console.log(event.target.value)
+  }
+
+  const handleDeposit = (event: any) => {
+    alert('You deposited ' + event.target[0].value + ' cents');
+    setBalance(balance + depositAmount)
+    console.log('click', event.target[0].value)
+    event.preventDefault()
+  }
+
   return(
     <div style={{border: '10px solid blue', margin: '10px', padding: '10px', display: 'inline-block'}}>
       <p>{jar.name}</p>
-      <p>Balance: {jar.balance} {jar.currency}</p>
-      <button>Deposit</button>
+      <p>Balance: {balance} {jar.currency}</p>
+      <form onSubmit={handleDeposit}>
+        <label>
+          Amount:
+          <input type="text" onChange={handleAmountChange} />
+        </label>
+        <input type="submit" value="Deposit" />
+      </form>
     </div> 
   )
 }
